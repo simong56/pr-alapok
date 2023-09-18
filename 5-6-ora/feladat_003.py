ad5c539927f896231e39a84d8de680d95a7af211
simong56 <113334762+simong56@users.noreply.github.com>
2023-09-18 14:14:22 +0200

    Add files via upload

---
+++ 5-6-ora/feladat_003.py
@@ -0,0 +1,10 @@
+#feladat_003
+#dk/95o
+
+print(f'Üdv néked!')
+évek_száma = input(f'Hány éves vagy? ')
+évek_száma = int(évek_száma)
+if évek_száma < 14:
+    print(f'Általános iskolába jársz.')
+else:
+    print(f'Egy év múlva {évek_száma+1} éves leszel.')
